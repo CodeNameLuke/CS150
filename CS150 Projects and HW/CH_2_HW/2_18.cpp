97c41c7a4507abeb4e6eae2fb0b6e95e8aaa3726
Lucas Silva <34361580+CodeNameLuke@users.noreply.github.com>
2018-02-07 14:13:22 -0800

    Update 2_18.cpp

--- CS150 Projects and HW/CH_2_HW/2_18.cpp
+++ CS150 Projects and HW/CH_2_HW/2_18.cpp
@@ -4,6 +4,8 @@ Write a program that asks the user to enter two integers, obtains the
 numbers from the user, then prints the larger number followed by the words "is larger." If the
 numbers are equal, print the message "These numbers are equal."
 
+THIS IS A MAJOR CHANGE
+
 
 **/
 
