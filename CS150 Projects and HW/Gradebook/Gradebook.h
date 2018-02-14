5bf4e94af500e74a9188a11faa4c899b7ed74529
Lucas Silva <34361580+CodeNameLuke@users.noreply.github.com>
2018-02-14 15:05:05 -0800

    Create Gradebook.h

---
+++ CS150 Projects and HW/Gradebook/Gradebook.h
@@ -0,0 +1,21 @@
+//Gradebook Interface Header.
+
+#include "string";
+
+class Gradebook {
+
+public:
+
+	explicit Gradebook(std::string name);
+	void setCourseName(std::string name);
+	std::string getCourseName() const;
+	void displayMessage() const;
+	void calcClassAverage() const;
+
+
+private:
+
+	std::string courseName;
+	std::string studentName;
+
+};
