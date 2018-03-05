e2ad4c2d3007555c6a655109f20672a28aefb543
Lucas Silva <34361580+CodeNameLuke@users.noreply.github.com>
2018-03-05 15:13:53 -0800

    Create GasMileage.cpp

---
+++ CS150 Projects and HW/CH_4_HW/GasMileage.cpp
@@ -0,0 +1,50 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+
+
+using namespace std;
+
+
+
+int main()
+{
+	bool test = true;
+
+	double milesDriven = 0;
+	double gallonsUsed = 0;
+	double mPG = 0;
+
+	double totalMilesDriven = 0;
+	double totalGallonsUsed = 0;
+	double totalMPG = 0;
+
+	while (milesDriven != -1) {
+
+		cout << "Enter miles driven (Enter -1 to exit): " << endl;
+		cin >> milesDriven;
+
+		if (milesDriven != -1) {
+
+			cout << "Enter gallons: " << endl;
+			cin >> gallonsUsed;
+
+			mPG = milesDriven / gallonsUsed;
+			totalMilesDriven = milesDriven + totalMilesDriven;
+			totalGallonsUsed = gallonsUsed + totalGallonsUsed;
+			totalMPG = totalMilesDriven / totalGallonsUsed;
+
+			cout << "MPG this trip: " << mPG << endl;
+			cout << "Total MPG: " << totalMPG << endl;		
+
+		}
+
+		cout << endl;
+		
+	}
+
+    return 0;
+}
+
