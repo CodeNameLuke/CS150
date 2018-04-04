7d30600be02cc63560ca23f285ffb02ed7380a5f
Lucas Silva <34361580+CodeNameLuke@users.noreply.github.com>
2018-04-04 15:01:39 -0700

    Create 5_12.cpp

---
+++ CS150 Projects and HW/CH_5_HW/5_12.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include "iostream"
+
+using namespace std;
+
+
+int main() {
+
+	int product = 1;
+
+	for (int i = 1; i <= 15; i += 2) {
+
+		product = product * i;
+
+	}
+
+	cout << product << endl;
+
+
+
+}
