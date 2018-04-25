7da5d3deeffba42d26b464b7df1b6a56ba24d50e
Lucas Silva <34361580+CodeNameLuke@users.noreply.github.com>
2018-04-25 14:39:50 -0700

    Update 6_12.cpp

--- CS150 Projects and HW/CH_6_HW/6_12.cpp
+++ CS150 Projects and HW/CH_6_HW/6_12.cpp
@@ -10,10 +10,33 @@ using namespace std;
 
 int main() {
 
-	double totalCost = calculateCharge(4.0);
+	double cost = calculateCharge(4.0);
+	double hours1 = 0.0, hours2 = 0.0, hours3 = 0.0, totalHours = 0.0;
+	double cost1 = 0.0, cost2 = 0.0, cost3 = 0.0, totalCost = 0.0;
 
-	cout << "Total Cost: " << fixed << setprecision(2) <<  totalCost << endl;
 
+	cout << "Enter Hour(s) For Car 1 : ";
+	cin >> hours1;
+	cout << "Enter Hour(s) For Car 2 : ";
+	cin >> hours2;
+	cout << "Enter Hour(s) For Car 3 : ";
+	cin >> hours3;
+
+
+	cost1 = calculateCharge(hours1);
+	cost2 = calculateCharge(hours2);
+	cost3 = calculateCharge(hours3);
+
+	totalCost = cost1 + cost2 + cost3;
+	totalHours = hours1 + hours2 + hours3;
+
+	cout << fixed;
+	cout << setprecision(2);
+	cout << "Car     Hours       Charge" << endl;
+	cout << " 1       " << hours1 << "        $" << cost1 << endl;
+	cout << " 2       " << hours2 << "        $" << cost2 << endl;
+	cout << " 3       " << hours3 << "        $" << cost3 << endl;
+	cout << "TOTAL    " << totalHours << "       $" << totalCost << endl;
 
 }
 
